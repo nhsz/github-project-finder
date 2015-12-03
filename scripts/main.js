4794a877ed7be042137c4bd8135a5bc4aa7cb874
Nicolás Quiroz <nquiroz@dc.uba.ar>
2015-12-03 12:02:08 -0300

    Add GitHub Search query

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 // results list
 var resultsList = $("#resultsList");
-resultsList.text("SADSASDASD");
+resultsList.text("ASD");
 
 // results button
 var toggleButton = $("#toggleButton");
@@ -17,5 +17,62 @@ toggleButton.on("click", function() {
         toggleButton.text("Hide results")
     };
 });
+
+//
+var githubSearch = "https://api.github.com/search/repositories?q=jquery+language:javascript&sort=stars"
+
+$.get(githubSearch)
+    .success(function(r) {
+        displayResults(r.items);
+})
+    .fail(function(err) {
+        console.log("Failed to query GitHub API.")
+})
+.done(function() {
+
+});
+
+//
+// var results = [{
+//     name: "jQuery",
+//     language: "JavaScript",
+//     score: 4.5,
+//     showLog: function () {
+//     },
+//     owner: {
+//         login: "shawnwildermuth",
+//         id: 123456
+// }
+// }, {
+//     name: "jQuery UI",
+//     language: "JavaScript",
+//     score: 3.5,
+//     showLog: function () {
+
+//     },
+//     owner: {
+//         login: "shawnwildermuth",
+//         id: 123456
+//     }
+// }];
+
+function displayResults(results) {
+    resultsList.empty();
+    $.each(results, function(i, item) {
+        var newResult = $("<div class = 'result'>" +
+            "<div class = 'title'>" + item.name + "</div>" +
+            "<div> Language: " + item.language + "</div>" +
+            "<div> Owner: " + item.owner.login + "</div>" +
+            "</div>" + "<br>");
+
+        newResult.hover(function() {
+            $(this).css("background-color", "#F5F5F5");
+        }, function() {
+            $(this).css("background-color", "transparent");
+        });
+
+        resultsList.append(newResult);
+    });
+}
 
 });
