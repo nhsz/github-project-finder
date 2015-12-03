2fff625618aedeeb1c8d7ef2c23384a43cd537ca
Nicolás Quiroz <nquiroz@dc.uba.ar>
2015-12-03 16:16:29 -0300

    Add GitHub API search with jQuery

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
 
 "use strict";
 
+
 // results list
 var resultsList = $("#resultsList");
-resultsList.text("ASD");
+resultsList.text("Insert your keywords, select the language and press the Search button.");
+
 
 // results button
 var toggleButton = $("#toggleButton");
@@ -18,52 +20,73 @@ toggleButton.on("click", function() {
     };
 });
 
+
 //
-var githubSearch = "https://api.github.com/search/repositories?q=jquery+language:javascript&sort=stars"
+$("#githubSearchForm").on("submit", function() {
 
-$.get(githubSearch)
-    .success(function(r) {
-        displayResults(r.items);
-})
-    .fail(function(err) {
-        console.log("Failed to query GitHub API.")
-})
-.done(function() {
+    var searchPhrase = $("#projectKeywords").val();
+    var sortBy = $("#sortBy").val();
+    var langChoice = $("#langChoice").val();
 
+    if (searchPhrase) {
+        resultsList.text("Searching...");
+
+        // repo keywords
+        var githubSearch = "https://api.github.com/search/repositories?q=" + encodeURIComponent(searchPhrase);
+
+        // choose repo language
+        if (langChoice != "Any language") {
+            githubSearch += "+language:" + encodeURIComponent(langChoice);
+        }
+
+        // sort results by stars, forks, updated
+        if (sortBy == "Stars") {
+            githubSearch += "&sort=stars";
+        } else if (sortBy == "Forks") {
+                githubSearch += "&sort=forks";
+        } else {
+            githubSearch += "&sort=updated";
+        }
+
+        // if there are no results, display an error message on console
+        $.get(githubSearch)
+            .success(function(r) {
+                displayResults(r.items);
+        })
+            .fail(function(err) {
+                console.log("Failed to query GitHub API.")
+        })
+        .done(function() {
+
+        });
+    }
+
+    return false;
 });
 
+
 //
-// var results = [{
-//     name: "jQuery",
-//     language: "JavaScript",
-//     score: 4.5,
-//     showLog: function () {
-//     },
-//     owner: {
-//         login: "shawnwildermuth",
-//         id: 123456
-// }
-// }, {
-//     name: "jQuery UI",
-//     language: "JavaScript",
-//     score: 3.5,
-//     showLog: function () {
-
-//     },
-//     owner: {
-//         login: "shawnwildermuth",
-//         id: 123456
-//     }
-// }];
-
 function displayResults(results) {
     resultsList.empty();
     $.each(results, function(i, item) {
+
+        var repoDescription = item.description;
+
+        if (repoDescription == "") {
+            repoDescription = "Description not provided.";
+        }
+
+        var repoUrl = "'" + item.html_url + "'";
+        var repoName =  "<a target='_blank' href=" + repoUrl + ">" + "<div class = 'title'>" + "<span class='octicon octicon-repo'></span> " + item.full_name + "</div>" + "<a/>";
+        var repoLanguage = "<strong><div> Language: </strong>" + item.language + "</div>";
+        var repoUpdatedOn = "<strong><div> Updated: </strong>" + item.updated_at + "</div>";
+
         var newResult = $("<div class = 'result'>" +
-            "<div class = 'title'>" + item.name + "</div>" +
-            "<div> Language: " + item.language + "</div>" +
-            "<div> Owner: " + item.owner.login + "</div>" +
-            "</div>" + "<br>");
+            repoName +
+            "<div>" + repoDescription + "</div>" +
+            repoLanguage +
+            repoUpdatedOn +
+            "<br>");
 
         resultsList.append(newResult);
     });
